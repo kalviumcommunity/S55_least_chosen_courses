ef55eafb276e927d2b96c5263caf7effb59ff642
Parthssh <naturallyinsane13@gmail.com>
2024-02-23 17:24:29 +0530

    added env and server

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const app = express();
-const port = 3080;
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+let connectionStatus = 'disconnected';
 
-app.get('/ping', (req, res) => {
-  res.send('Hello Kalvian');
+const startDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MongoURI);
+        connectionStatus = "The database is connected successfully ";
+    } catch (err) {
+        console.error("Failed to connect to database");
+        connectionStatus = "error";
+    }
+};
+
+const stopDatabase = async () => {
+    await mongoose.disconnect();
+    connectionStatus = "closed";
+};
+
+app.get('/', (req, res) => {
+    res.send(connectionStatus);
 });
 
+app.get("/ping", (req, res) => {
+    res.send('Hello');
+});
 
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`server running on PORT: ${port}`);
-  });
-}
+app.listen(3080, () => {
+    startDatabase();
+    console.log('Port 3080');
+});
 
 module.exports = app;
