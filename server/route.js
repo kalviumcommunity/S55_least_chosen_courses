1b5381f337310eb885bfaa1a63846d02ee9d4e02
Parthssh <naturallyinsane13@gmail.com>
2024-03-13 17:51:34 +0530

    changes

--- server/route.js
+++ server/route.js
@@ -1,96 +1,91 @@
-const express = require('express')
-const { UserModel} = require('./schema')
-const router = express.Router()
-const Joi = require('joi');
+const express = require("express");
+const { UserModel } = require("./schema");
+const router = express.Router();
+const Joi = require("joi");
 
-
-router.use(express.json())
+router.use(express.json());
 
 const NewSchema = Joi.object({
-    name: Joi.string().required(),
-    duration: Joi.string().required(),
-    ratings: Joi.string().required(),
-    imageLink: Joi.string().required()
+  name: Joi.string().required(),
+  duration: Joi.string().required(),
+  ratings: Joi.string().required(),
+  imageLink: Joi.string().required(),
 });
 
-router.get('/get',(req,res)=>{
-    res.send('get request')
-})
-
-router.post('/post',(req,res)=>{
-    console.log(req.body)
-    res.json(req.body)
-})
-
-router.put('/put',(req,res)=>{
-    res.send("put request")
-})
-
-router.delete('/delete',(req,res)=>{
-    res.send("delete request")
-})
-
-
-router.get('/course',async(req,res) =>{
-    try{
-        const test = await UserModel.find({})
-        res.json(test)
-
-    }catch(err){
-        console.log(err)
-    }
-})
-
-
-router.post('/add', async (req, res) => {
-    try {
-        const { error, value } = NewSchema.validate(req.body);
-        if (error) {
-            return res.status(400).send(error.details[0].message);
-        }
-        const newData = await UserModel.create(req.body);
-        res.send(newData);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error');
-    }
+router.get("/get", (req, res) => {
+  res.send("get request");
 });
 
+router.post("/post", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
 
-router.get('/course/:id', async (req,res) => {
-    const _id = req.params.id
-    UserModel.findById({_id})
-    .then(users => res.json(users))
-    .catch(err => console.log(err))
-})
+router.put("/put", (req, res) => {
+  res.send("put request");
+});
 
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const _id = req.params.id;
-        const deleteUser = await UserModel.findByIdAndDelete(_id);
-        if (!deleteUser) {
-            return res.status(404).send('User not found');
-        }
-        res.json(deleteUser);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error');
+router.delete("/delete", (req, res) => {
+  res.send("delete request");
+});
+
+router.get("/course", async (req, res) => {
+  try {
+    const test = await UserModel.find({});
+    res.json(test);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/add", async (req, res) => {
+  try {
+    const { error, value } = NewSchema.validate(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
     }
+    const newData = await UserModel.create(req.body);
+    res.send(newData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error");
+  }
+});
+
+router.get("/course/:id", async (req, res) => {
+  const _id = req.params.id;
+  UserModel.findById({ _id })
+    .then((users) => res.json(users))
+    .catch((err) => console.log(err));
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const deleteUser = await UserModel.findByIdAndDelete(_id);
+    if (!deleteUser) {
+      return res.status(404).send("User not found");
+    }
+    res.json(deleteUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error");
+  }
 });
 
 router.put(`/updateCard/:id`, async (req, res) => {
-    try {
-        const { error, value } = NewSchema.validate(req.body);
-        if (error) {
-            return res.status(400).send(error.details[0].message);
-        }
-        const _id = req.params.id;
-        const updateUser = await UserModel.findByIdAndUpdate({_id:_id},value);
-        res.json(updateUser);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error');
+  try {
+    const { error, value } = NewSchema.validate(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
     }
+    const _id = req.params.id;
+    const updateUser = await UserModel.findByIdAndUpdate({ _id: _id }, value);
+    res.json(updateUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error");
+  }
 });
 
-module.exports = router
+module.exports = router;
